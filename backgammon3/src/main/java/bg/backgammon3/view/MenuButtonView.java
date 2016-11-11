00d5e19406de0e610d3793c27d9762f40861dd44
Philipp Herkert <philipp.herkert@gmail.com>
2016-11-11 12:22:38 +0100

    Menu Hover Effekt hinzugefügt. Spiel etwas vergrößert

--- backgammon3/src/main/java/bg/backgammon3/view/MenuButtonView.java
+++ backgammon3/src/main/java/bg/backgammon3/view/MenuButtonView.java
@@ -6,6 +6,7 @@ package bg.backgammon3.view;
 import bg.backgammon3.config.Config;
 import bg.backgammon3.model.*;
 import bg.backgammon3.view.helper.StaticImageHelper;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
@@ -18,6 +19,12 @@ import javafx.scene.text.Text;
 public class MenuButtonView extends ImageView implements GameObjectView {
 	private MenuButton menuButton;
 	private Text menu;
+	
+
+	private Image normalImage;
+	
+	private Image hoverImage;
+	
 	public MenuButtonView(MenuButton menuButton,Pane root)
 	{
 		menu = new Text();
@@ -36,7 +43,24 @@ public class MenuButtonView extends ImageView implements GameObjectView {
 
 		// MouseClick wird auch auf transperentem Hintergrund ausgelöst.
 		setPickOnBounds(true);
+		
+		normalImage = this.getImage();
+		hoverImage = StaticImageHelper.loadImage(Config.getString("menuButtonImageHover"));
+		
+		// Hover effekt
+		this.setOnMouseEntered(e->{
+			setImage(hoverImage);
+		});
+		
+		this.setOnMouseExited(e -> {
+			setImage(normalImage);
+		});
+		
+		this.setOnMouseMoved(e -> {
+			setImage(hoverImage);
+		});
 	}
+	
 	
 	@Override
 	public GameObject getGameObject() {
