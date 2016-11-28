6e76de8ef070bbd64066c51263ea96b18210904c
Philipp Herkert <philipp.herkert@gmail.com>
2016-11-28 22:45:16 +0100

    Kleinere Bugs entfernt.

--- backgammon3/src/main/java/bg/backgammon3/view/helper/BackGroundHelper.java
+++ backgammon3/src/main/java/bg/backgammon3/view/helper/BackGroundHelper.java
@@ -13,8 +13,11 @@ import bg.backgammon3.config.Config;
 import javafx.animation.*;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
+import javafx.scene.ImageCursor;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
@@ -34,22 +37,31 @@ import javafx.util.Duration;
 public class BackGroundHelper {
 	private static int tmpNode = 0;
 	private static int tmpNode2 = 0;
+	private static int tmpNode3 = 0;
 	private static int scale = 0;
 	private static boolean show = true;
 	private static boolean mu = false;
+	private static boolean mhd = false;
 	private static ArrayList<Node> elemente = new ArrayList<Node>();
+	private static ArrayList<MediaPlayer> mediaPlayer = new ArrayList<MediaPlayer>();
+	private static int count = 0;
+	private static ImageCursor image;
 
 	
-	public static void showBackground(Pane root, ImageView bgv) {
-		Media media = new Media(new File(Config.getString("happySound")).toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.setCycleCount(0);
+	public static void showBackground(Pane root, ImageView bgv, int dir, double y, double v, double s) {
 		
 		tmpNode = 0;
 		double bodySizeY = 20;
 		double bodyPositionY = 20;
 		double startX = -root.getWidth()/root.getScaleX() / 2 - 40;
-		double startY = root.getHeight()/root.getScaleY() / 2 - bodySizeY - bodyPositionY;
+		if(dir == 0) {
+			startX = -root.getWidth()/root.getScaleX() / 2 - 60;
+		} else {
+			startX = +root.getWidth()/root.getScaleX() / 2 + 60;
+		}
+		double startY;
+		startY = root.getHeight()/root.getScaleY() / 2 - bodySizeY - bodyPositionY - y * root.getHeight()/root.getScaleY();
+		
 		Group g = new Group();
 		g.setTranslateX(startX);
 		g.setTranslateY(startY);
@@ -104,6 +116,9 @@ public class BackGroundHelper {
 		//t.setStroke(Color.BLACK);
 		g.getChildren().add(t);
 
+		g.setScaleX(s);
+		g.setScaleY(s);
+		
         g.setPickOnBounds(true);
 		g.setMouseTransparent(false);
 		
@@ -111,25 +126,27 @@ public class BackGroundHelper {
 		elemente.add(g);
 		g.toFront();
 
+		g.setVisible(show);
 		t.toFront();
 		
 		Path path = new Path();
 		double jumpLength = 100;
 		int numberJumps = 80;
 		double slope = 50;
+		int direction = dir == 0 ? 1 : -1;
 		for(int i = 0; i < numberJumps; i++) {
-			path.getElements().add(new MoveTo(startX + jumpLength * i, startY));
+			path.getElements().add(new MoveTo(startX + jumpLength * i * direction, startY));
 			path.getElements().add(new CubicCurveTo(
-					startX + jumpLength * i + slope, 
+					startX + (jumpLength * i + slope) * direction, 
 					startY - jumpLength, 
-					startX + jumpLength * (i + 1) - slope, 
+					startX + (jumpLength * (i + 1) - slope) * direction, 
 					startY - jumpLength, 
-					startX + jumpLength * (i + 1), 
+					startX + (jumpLength * (i + 1)) * direction, 
 					startY));
 		}
 		PathTransition pathTransition = new PathTransition();
 		
-		pathTransition.setDuration(Duration.millis(40000));
+		pathTransition.setDuration(Duration.millis(40000*v));
 		pathTransition.setNode(g);
 		pathTransition.setPath(path);
 		pathTransition.setCycleCount(1);
@@ -139,13 +156,16 @@ public class BackGroundHelper {
 		t.translateYProperty().bind(c.translateYProperty().add(c.radiusYProperty().getValue() / 2));
 		pathTransition.setOnFinished(a -> {
 			root.getChildren().remove(g);
+			elemente.remove(g);
 		});
 		pathTransition.play();
 		
 
 		g.setOnMouseClicked(a -> {
-			if(mu) {
-				mediaPlayer.play();
+			if(mu && g.getChildren().get(0).isVisible()) {
+				mediaPlayer.get(count).stop();
+				mediaPlayer.get(count).play();
+				count = (count + 1) % mediaPlayer.size();
 			}
 			for(int i = 0; i < g.getChildren().size(); i++) {
 				g.getChildren().get(i).setVisible(false);
@@ -157,20 +177,67 @@ public class BackGroundHelper {
 		});
 	}
 	
-	public static void toggleVisibility() {
+
+	public static void showBackground(Pane root, ImageView bgv) {
+		showBackground(root, bgv, 0, 0, 1, 1);
+	}
+	
+	public static void activateInstantAction(Pane root, ImageView bgv) {
+		root.setCursor(image);
+		ArrayList<KeyFrame> kf = new ArrayList<KeyFrame>();
+		scale = scale > 180 ? 180 : scale;
+		scale = scale < 30 ? 30 : scale;
+		double time = 0;
+		Random r = new Random();
+		while(time < scale * 1000) {
+			time += r.nextInt(1000) + 200.0;
+			kf.add(new KeyFrame(Duration.millis(time), a -> {
+				int dir = r.nextInt(2);
+				double y = r.nextDouble() * 0.75;
+				double v = r.nextDouble() * 0.6 + 0.5;
+				double s = r.nextDouble() + 0.5;
+
+				showBackground(root, bgv, dir, y, v, s);
+			}));
+		}
+		kf.add(new KeyFrame(Duration.millis(time + 3000), a -> {mhd = false; root.setCursor(Cursor.DEFAULT); }));
+		Timeline timeline  = new Timeline(); 
+		timeline.setCycleCount(0); 
+		timeline.setAutoReverse(false); 
+		timeline.getKeyFrames().addAll(kf); 
+		timeline.play();
+		
+		scale = 0;
+	}
+	
+	public static void toggleVisibility(Pane root) {
 		show = !show;
 		for(int i = 0; i < elemente.size(); i++) {
 			elemente.get(i).setVisible(show);
 		}
+		if(show) {
+			root.setCursor(image);
+		} else {
+			root.setCursor(Cursor.DEFAULT);
+		}
 	}
 	
 	public static void initBackground(Pane root, ImageView bgv) {
+		Media media = new Media(new File(Config.getString("happySound")).toURI().toString());
+		for(int i = 0; i < 10; i++) {
+			mediaPlayer.add(new MediaPlayer(media));
+			mediaPlayer.get(i).setCycleCount(0);
+		}
+		image = new ImageCursor(StaticImageHelper.loadImage(Config.getString("happyVision")));
+
+		
 		bgv.setOnKeyTyped(e -> {
 			if(e.getCharacter().equals("s")){
-				toggleVisibility();
+				toggleVisibility(root);
 			}
 			if(tmpNode == 5) {
 				if(e.getCharacter().equals("r")) {
+					root.setCursor(image);
 					ArrayList<KeyFrame> kf = new ArrayList<KeyFrame>();
 					scale = scale > 30 ? 30 : scale;
 					if(scale == 0) {
@@ -184,6 +251,7 @@ public class BackGroundHelper {
 							showBackground(root, bgv);
 						}));
 					}
+					kf.add(new KeyFrame(Duration.millis(time + 3000), a -> {root.setCursor(Cursor.DEFAULT); }));
 					Timeline timeline  = new Timeline(); 
 					timeline.setCycleCount(0); 
 					timeline.setAutoReverse(false); 
@@ -242,6 +310,22 @@ public class BackGroundHelper {
 			if(e.getCharacter().equals("m")) {
 				tmpNode2 = 1;
 			}
+			
+
+			if(tmpNode3 == 1) {
+				if(e.getCharacter().equals("c")) {
+					if(!mhd) {
+						mhd = true;
+						activateInstantAction(root, bgv);
+					}
+				} else {
+					tmpNode3 = 0;
+				}
+			}
+			if(e.getCharacter().equals("q")) {
+				tmpNode3 = 1;
+			}
+			
 			//if(e.getCharacter().equals("m"))
 			if(e.getCharacter().charAt(0) >= '0' && e.getCharacter().charAt(0) <= '9')
 			{
